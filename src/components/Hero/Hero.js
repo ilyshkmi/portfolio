54044cfff0ccd689e1a60f92a1691ad4ae991551
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-11-23 16:04:19 +0800

    feat: added accomplishment, project, about me, tech and timeline pages

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle maincenter>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a Front End and Mobile App Developer
+      </SectionText>
+      <Button onClick={props.handleClick}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
