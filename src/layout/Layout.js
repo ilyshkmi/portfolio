cbf2b1580d261c0987ca25553393b99b3a0b0a77
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-12-17 23:11:17 +0800

    feat: add head title, tab title

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
@@ -7,9 +8,13 @@ import { Container } from './LayoutStyles'
 export const Layout = ({children}) => {
   return (
     <Container>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
+      <Head>
+        <title>My Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Header/>
+      <main>{children}</main> 
+      <Footer/>
     </Container>
   )
 }
