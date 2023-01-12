0157c417a72aa7f119c7412185657fa684849660
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2023-01-12 17:46:21 +0800

    feat: update hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-      My Name is Muhd Saiful. I am a Front End and Mobile App Developer. Currently working at Azure Innovations Sdn Bhd as a Software Developer.
+        Hi, My Name is Muhd Saiful. I am a Front End and Mobile App Developer. Currently working at Azure Innovations Sdn Bhd as a Software Developer.
       </SectionText>
       <Button href="https://saiful-personal-bucket.s3.us-east-2.amazonaws.com/Resume.pdf">Get Resume</Button>
     </LeftSection>
