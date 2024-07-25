1374382162d1eff40ba74765e077454a2b903938
ilyshkmi <145740235+ilyshkmi@users.noreply.github.com>
2024-07-25 08:01:20 +0800

    Update Hero.js
    update personal portfolio

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,8 @@ const Hero = (props) => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-        Hello, Ilyas Hakimi here. Fresh graduate from Universiti Malaysia Pahang Al-Sultan Abdullah. Recently completed my Industrial Training at Sony EMCS (Malaysia) Sdn Bhd as Web Developer. 
+      Hello, Ilyas Hakimi here. Experienced IT Officer at Nissha Precision Technologies Malaysia Sdn. Bhd. Expertise in managing and maintaining IT infrastructure, providing technical support, and implementing security measures. Adept at troubleshooting, network management, and IT projects. Committed to ensuring optimal system performance and data security.
+      Graduate from Universiti Malaysia Pahang Al-Sultan Abdullah. Completed my Industrial Training at Sony EMCS (Malaysia) Sdn Bhd as Web Developer. 
       </SectionText>
       <Button href="https://drive.google.com/file/d/1Q-wazPvzoIXn_qGmQUknb10uTJCTyIf2/view?usp=sharing">Get Resume</Button>
     </LeftSection>
